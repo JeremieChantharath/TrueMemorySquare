63b0076c2f06b53be7468098235f877132463f47
Jeremie Chantharath <jeremie82@live.fr>
2018-01-22 00:52:14 +0100

    Jeu fonctionne !
    ajout de printinfo
    modification gestion de vie : 3 vies "bonus" par niveau
    gestion des generates amélioré
    userEntry de l'activity utile et est une ArrayList (sert à comparer avec la suite de Game)
    coeur du jeu dans le onclick des boutons (bonne/mauvaise chose ?)

--- app/src/main/java/com/example/chanthaj/truememorysquare/FullscreenActivity.java
+++ app/src/main/java/com/example/chanthaj/truememorysquare/FullscreenActivity.java
@@ -1,23 +1,25 @@
 package com.example.chanthaj.truememorysquare;
 
-import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
-import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class FullscreenActivity extends AppCompatActivity {
 
     private static final String TAG = "MyActivity";
-    private ArrayList<Integer> userEntry;
+    private ArrayList<Integer> userEntries;
     private Game game;
 
     @Override
@@ -27,15 +29,43 @@ public class FullscreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fullscreen);
 
         this.game = new Game();
-        Log.v(TAG,"NUMBER " + game.getSuite()[0]);
-        generateButtons(this.game.getNumButtons());
+        this.userEntries = new ArrayList<>();
+
+
+        generateWithDelay();
+    }
+
+    public void printInfos(){
+
+        TextView score = (TextView)findViewById(R.id.score);
+        score.setText("Score :" + this.game.getScore());
+
+        TextView level = (TextView)findViewById(R.id.level);
+        level.setText("Level :" + this.game.getLevel());
+
+        TextView levelLife = (TextView)findViewById(R.id.levelLife);
+        levelLife.setText("LevelLife :" + this.game.getLevelLife());
+
+        TextView life = (TextView)findViewById(R.id.life);
+        life.setText("Life :" + this.game.getLife());
+    }
+
+    public void generateWithDelay(){
+
+        generateButtonsToMemorize(this.game.getNumButtons());
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                generateButtons(game.getNumButtons());
+            }
+        }, 3000);
     }
 
     public void generateButtons(int numButtons) {
 
-        //Ca c'est l'id du button
         int buttonId = 1;
-
         RelativeLayout rl = (RelativeLayout) findViewById(R.id.relativelayout);
         rl.removeAllViews();
 
@@ -44,20 +74,18 @@ public class FullscreenActivity extends AppCompatActivity {
             //Ca ça sert à les placer sur une colonne
             for (int j = 1; j <= numButtons; j++) {
 
-
-                //Ca créer un bouton
                 Button btn = new Button(this);
-                //Donne l'id au bouton, pour ensuite pouvoir le différencier des autres, chaque boutons aura un id différent
                 btn.setId(buttonId);
 
-                //Donne une couleur au bouton (osef)
+                //Couleur de base
                 btn.setBackgroundColor(Color.rgb(70, 80, 90));
 
-                //Créer les paramètres que l'on donnera au bouton
+                //Créer l'objet pour les paramètres du bouton
                 RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 
-                //Donne les dimensions au bouton (ils auront tous la même taille)
+                //methode perso, donne les dimensions au bouton
                 setButtonsParams(p);
+
                 //Verifie si le bouton est le premier de la colonne, s'il ne l'est pas on place ce bouton à DROITE du bouton précédent
                 if (!isInFirstColomn(buttonId, numButtons))
                     p.addRule(RelativeLayout.RIGHT_OF, buttonId - 1);
@@ -68,14 +96,70 @@ public class FullscreenActivity extends AppCompatActivity {
                 //Donne les paramètres au bouton
                 btn.setLayoutParams(p);
 
-                //OSEf on s'en sert pas
+
                 btn.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View view) {
-                        view.setBackgroundColor(Color.rgb(70, 0, 0));
-                        game.verif(view.getId());
-                        Log.v(TAG,"LEVEL " + game.getLevel());
-                        Log.v(TAG,"LIFE " + game.getLife());
+                        Log.v(TAG,"*********************** NEW BUTTON PRESSED *********************** ");
 
+                        Log.v(TAG,"PRESSED " + view.getId());
+                        Log.v(TAG," SUITE " + game.getStringSuite());
+
+                        Log.v(TAG," USER ENTRIES " + game.getLife());
+
+                        Log.v(TAG,"LEVEL " + game.getLevel());
+                        Log.v(TAG,"LEVEL LIFE " + game.getLevelLife());
+                        Log.v(TAG," LIFE " + game.getLife());
+
+
+                        // un essai nécessaire
+                        view.setClickable(false);
+
+                        //En cas de bonne réponse
+                        if(game.verifCarre(view.getId())){
+                            //Ajoute à la liste des réponses du joueur
+                            userEntries.add(view.getId());
+                            view.setBackgroundColor(Color.rgb(70, 200, 70));
+
+                            if(game.nextLevel(userEntries))
+                            {
+                                game.setLevel(game.getLevel()+1);
+                                userEntries.clear();
+                                game.createSuite();
+                                final Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        generateWithDelay();
+                                    }
+                                }, 1000);
+                            Toast.makeText(getApplicationContext(),"Bien joué !",Toast.LENGTH_SHORT).show();
+                            }
+
+                         //En cas de mauvaise
+                        }else{
+                            view.setBackgroundColor(Color.rgb(200, 0, 0));
+                            game.setLevelLife(game.getLevelLife()-1);
+                            if(game.levelLost())
+                            {
+                                game.setLevel(game.getLevel()-1);
+                                game.setLife(game.getLife()-1);
+                                if(game.gameOver())
+                                {
+                                    Intent intent = new Intent(getApplicationContext(), GameOver.class);
+                                    startActivity(intent);
+                                }
+                                final Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        generateWithDelay();
+                                    }
+                                }, 1000);
+                                Toast.makeText(getApplicationContext(),"Dommage..",Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                        printInfos();
                     }
                 });
                 //Recupère la zone blanche du screen
@@ -87,9 +171,40 @@ public class FullscreenActivity extends AppCompatActivity {
         }
     }
 
-    public void addToUserEntry(int entree){
-        this.userEntry.add(entree);
+    //TODO faire les generate comme il faut
+    public void generateButtonsToMemorize(int numButtons) {
+
+        int buttonId = 1;
+
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.relativelayout);
+        rl.removeAllViews();
+
+        for (int i = 1; i <= numButtons; i++) {
+            for (int j = 1; j <= numButtons; j++) {
+                Button btn = new Button(this);
+                btn.setId(buttonId);
+                //Si fait partit de la suite rouge, sinon comme les autres
+                if(this.game.getSuite().contains(buttonId))
+                    btn.setBackgroundColor(Color.rgb(70, 0, 0));
+                else
+                    btn.setBackgroundColor(Color.rgb(70, 80, 90));
+
+                RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+                //On ne peut pas commencer à jouer avant le début
+                btn.setClickable(false);
+                setButtonsParams(p);
+                if (!isInFirstColomn(buttonId, numButtons))
+                    p.addRule(RelativeLayout.RIGHT_OF, buttonId - 1);
+                if(i>1)
+                    p.addRule(RelativeLayout.BELOW, buttonId - numButtons);
+                btn.setLayoutParams(p);
+                rl.addView(btn);
+                buttonId++;
+            }
+        }
     }
+
 
     public void setButtonsParams(RelativeLayout.LayoutParams p) {
         p.leftMargin = 50;
--- app/src/main/java/com/example/chanthaj/truememorysquare/Game.java
+++ app/src/main/java/com/example/chanthaj/truememorysquare/Game.java
@@ -1,6 +1,5 @@
 package com.example.chanthaj.truememorysquare;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
@@ -10,6 +9,7 @@ public class Game {
     private ArrayList<Integer> suite;
     private int score;
     private int level;
+    private int levelLife;
     private int life;
     //key = level, value = number of columns/lines (it's a square)
     private HashMap<Integer,Integer> numberOfButton;
@@ -17,56 +17,57 @@ public class Game {
     public Game(){
         this.score=0;
         this.level=1;
-        this.life=3;
+        this.levelLife = 3;
+        this.life = 3;
 
         this.numberOfButton= new HashMap<>();
         this.numberOfButton.put(1,3);
         this.numberOfButton.put(2,3);
         this.numberOfButton.put(3,4);
         this.numberOfButton.put(4,4);
+        this.numberOfButton.put(5,5);
+        this.numberOfButton.put(6,5);
+        this.numberOfButton.put(7,6);
+        this.numberOfButton.put(8,6);
+        this.numberOfButton.put(9,7);
+        this.numberOfButton.put(10,7);
 
         this.suite= new ArrayList<>();
         createSuite();
 
     }
 
-    private void debutTour(){
-
+    public boolean gameOver(){
+       return this.life<=0;
     }
 
-    private int getNumberOfButton(){
-        return this.numberOfButton.get(this.level);
+    public boolean levelLost(){
+        return this.levelLife<=0;
     }
 
-    private void createSuite(){
+    public boolean nextLevel(ArrayList<Integer> userEntries){
+        return userEntries.size() == this.suite.size();
+    }
+
+    public void createSuite(){
         this.suite.clear();
         Random r = new Random();
         for (int i = 1; i <= this.level*2 ; i++) {
-            this.suite.add(r.nextInt(10));
+            //-1)+1 ----> +1 en dehors du random car doit pas etre 0, -1 car doit etre en 1 et le nombre de carrés qu'il y aura et non le nb de carré+1
+            int newEntry = r.nextInt(getNumButtons()*getNumButtons()-1)+1;
+            while(this.suite.contains(newEntry))
+                newEntry = r.nextInt(getNumButtons()*getNumButtons()-1)+1;
+            this.suite.add(newEntry);
         }
     }
 
-
-    private void nextLevel(){
-        this.level++;
-    }
-    private void levelBefore(){
-        if(this.level > 1)
-            this.level--;
-    }
-
-    public void verif(int userEntry){
-        if(this.suite.contains(userEntry)){
-            nextLevel();
-        }
-        else{
-            levelBefore();
-            this.life--; //TODO: fonction à part
-        }
+    //Verifie si le carré appuyé est bon
+    public boolean verifCarre(int userEntry){
+        return this.suite.contains(userEntry);
     }
 
     public void calculScore(){
-        //TODO
+        this.score = this.score + this.level;
     }
 
 
@@ -87,6 +88,18 @@ public class Game {
         this.level = level;
     }
 
+    public int getLevelLife() {
+        return levelLife;
+    }
+
+    public void setLevelLife(int levelLife) {
+        this.levelLife = levelLife;
+    }
+
+    public int getNumButtons(){
+        return this.numberOfButton.get(this.level);
+    }
+
     public int getLife() {
         return life;
     }
@@ -95,15 +108,17 @@ public class Game {
         this.life = life;
     }
 
-    public int getNumButtons(){
-        return this.numberOfButton.get(this.level);
+
+    public ArrayList<Integer> getSuite(){
+        return this.suite;
     }
 
-    public int[] getSuite(){
-        int[] res= new int[this.suite.size()];
-        for(int i=0; i <this.suite.size(); i++){
-            res[i] = this.suite.get(i);
+    public String getStringSuite(){
+        String s ="";
+        for(int i = 0; i < this.suite.size(); i++)
+        {
+            s += Integer.toString(this.suite.get(i)) + " ";
         }
-        return res;
+        return s;
     }
 }
